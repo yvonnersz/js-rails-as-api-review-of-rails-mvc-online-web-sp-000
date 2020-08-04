8365abb0b1a4afab3a1a10729034aacf4837f74d
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-04 13:55:54 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Add route from Readme
 
   Rails.application.routes.draw do
-  get '/birds' => 'birds#index'
-end
+    get '/birds' => 'birds#index'
+  end
 
 end
